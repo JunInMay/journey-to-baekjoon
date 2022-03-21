882daa384c817af51434a861af3f90675a988ff2
Joon <shuangjutou@naver.com>
2022-03-21 11:12:07 +0900

    2022-03-21-01

--- baekjoon/main.py
+++ baekjoon/main.py
@@ -1,15 +1,2 @@
-import sys
-
-
-A = int(sys.stdin.readline().rstrip())
-B = int(sys.stdin.readline().rstrip())
-C = int(sys.stdin.readline().rstrip())
-
-
-l = [0 for _ in range(10)]
-
-for c in str(A * B * C):
-    l[int(c)] += 1
-
-for c in l:
-    print(c)
+print(-1//3)
+print(-3//2)
