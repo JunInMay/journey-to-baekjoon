85898097f196b15143779ad083def68b10749c01
Joon <shuangjutou@naver.com>
2022-04-21 23:05:03 +0900

    2022-04-21-01

--- baekjoon/main.py
+++ baekjoon/main.py
@@ -1,24 +1,6 @@
-"""
-# 24883
 import sys
 
-if sys.stdin.readline().rstrip() in ['N', 'n']:
-    print("Naver D2")
-else:
-    print("Naver Whale")
-"""
-
-import sys
-
-input_number = sys.stdin.readline().rstrip()
-F = int(sys.stdin.readline().rstrip())
-
-for i in range(100):
-    if i < 10:
-        res = "0" + str(i)
-    else:
-        res = str(i)
-    N = int(input_number[:-2] + res)
-    if N % F == 0:
-        print(res)
-        break
+res = 0
+for _ in range(5):
+    res += int(sys.stdin.readline().rstrip())
+print(res)
