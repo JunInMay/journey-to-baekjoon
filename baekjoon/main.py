fdb7354493e5d258f3793b8f9140f110f892e1e5
Joon <shuangjutou@naver.com>
2022-06-02 22:04:13 +0900

    2022-06-02-01

--- baekjoon/main.py
+++ baekjoon/main.py
@@ -1,7 +1,4 @@
 import sys
 
-hour, minute, second = map(int, sys.stdin.readline().rstrip().split())
-passed_time = int(sys.stdin.readline().rstrip())
-time = hour * 3600 + minute * 60 + second + passed_time
-
-print(time // 3600 % 24, time % 3600 // 60, time % 3600 % 60 % 60)
+A, B = map(int, sys.stdin.readline().rstrip().split())
+print(A * B)
